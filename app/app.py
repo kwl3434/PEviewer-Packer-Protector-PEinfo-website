c4001579ae14e68b49e9c0beff0f2779adf03467
kwl3434 <kwleepuppy@naver.com>
2019-03-29 22:55:26 +0900

    Ubuntu Flask

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,redirect, url_for, request
+from flask import Flask,render_template,redirect
 app = Flask(__name__)
 
 @app.route('/')
--- app/venv/bin/activate.fish
+++ app/venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/app/venv"
+set -gx VIRTUAL_ENV "/PEviewer-Packer-Protector-PEinfo-website/app/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
