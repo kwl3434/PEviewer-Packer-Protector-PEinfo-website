89a9afd4915820b3733bfd6ea5867a03c56dc920
kwl3434 <kwleepuppy@naver.com>
2019-04-08 20:47:15 +0900

    virustotal API script

---
+++ app/templates/upload.php
@@ -0,0 +1,47 @@
+<?php
+$target_dir = "/share";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+// if(isset($_POST["submit"])) {
+//     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+//         if($check !== false) {
+//                 echo "File is an image - " . $check["mime"] . ".";
+//                         $uploadOk = 1;
+//                             } else {
+//                                     echo "File is not an image.";
+//                                             $uploadOk = 0;
+//                                                 }
+//                                                 }
+//                                                 // Check if file already exists
+//                                                 if (file_exists($target_file)) {
+//                                                     echo "Sorry, file already exists.";
+//                                                         $uploadOk = 0;
+//                                                         }
+//                                                         // Check file size
+//                                                         if ($_FILES["fileToUpload"]["size"] > 5000000) {
+//                                                             echo "Sorry, your file is too large.";
+//                                                                 $uploadOk = 0;
+//                                                                 }
+//                                                                 // Allow certain file formats
+//                                                                 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+//                                                                 && $imageFileType != "gif" ) {
+//                                                                     echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+//                                                                         $uploadOk = 0;
+//                                                                         }
+//                                                                         // Check if $uploadOk is set to 0 by an error
+//                                                                         if ($uploadOk == 0) {
+//                                                                             echo "Sorry, your file was not uploaded.";
+//                                                                             // if everything is ok, try to upload file
+//                                                                             } else {
+//                                                                                 if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+//                                                                                         echo "<p>The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.</p>";
+//                                                                                                      echo "<br><img src=/uploads/". basename( $_FILES["fileToUpload"]["name"]). ">";
+//                                                                                                                      echo "<br><button type='button' onclick='history.back()'>돌아가기</button>";
+//                                                                                                                          } else {
+//                                                                                                                                  echo "<p>Sorry, there was an error uploading your file.</p>";
+//                                                                                                                                              echo "<br><button type='button' onclick='history.back()'>돌아가기</button>";
+//                                                                                                                                                  }
+//                                                                                                                                                  }
+//                                                                                                                                                  ?>
