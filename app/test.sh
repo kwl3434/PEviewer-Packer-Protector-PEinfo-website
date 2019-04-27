296d49434cddb7c7cb3a05d4e20f3caa28c789ed
kwl3434 <kwleepuppy@naver.com>
2019-04-27 18:58:04 +0900

    19.4.27

--- app/test.sh
+++ app/test.sh
@@ -1,2 +1,11 @@
- if [wc -c output.txt | awk '{print $1}' -gt 232 -a  wc -c ouput1.txt | awk '{print $1}' ]
-         print("gwnu")
+#!/bin/bash
+value = wc -c /root/ouput.txt | awk '{print $1}' 
+if [ $value -gt 232 ]; then  # 바이러스없는 출력 결과물의 바이트 수보다 크면 바이러스 파일이므로라 삭제
+          #sed -e '1,$d' /root/test1.txt > /root/test1.txt
+          #rm ./uploadfile/*.* #바이러스 파일이므로 파일 삭제
+       echo "this is  if test"
+elif[ $value -le 232 ]; then #바이러스없는 출력 결과물의 바이트 수가 작거나 같으면 아무 이상 없음
+          echo "this is  if test"                                                       #PEviwer PEinfo 실행
+         # rm ./uploadfile/*.* #업로드된 파일을 모두 지우는 이유는 업로드경로상에 파일이 남아있으면 test1.txt에 해쉬값이 계속 쌓이기 때문에
+                            #바이러스 토탈에서 바이러스가 있든 없든 같은 파일 해쉬값을 분석 하기 때문
+ fi
--- app/upload.py
+++ app/upload.py
@@ -7,9 +7,11 @@ from flask import send_file
 from werkzeug import secure_filename
 app = Flask(__name__)
 main='main.html'
-pack='pack_protector.html'
+pack='pack_protector'
 howto='howtouse.html'
 total='total.html'
+peinfo='peinfo'
+peview='peview'
 jpg1='1.JPG'
 jpg2='2.JPG'
 jpg3='3.JPG'
@@ -24,7 +26,7 @@ def main():
 
 @app.route('/total')
 def total():
-    return render_template('total.html',src1=pack)
+    return render_template('total.html',src1=pack,src2=peinfo, src3=peview)
 
 @app.route('/home')
 def home():
@@ -34,10 +36,17 @@ def home():
 def howtouse():
     return render_template('howtouse.html')
 
-@app.route('/pack_protector.html', methods = ['GET','POST'])
+@app.route('/peinfo')
+def peinfo_e():
+    return render_template('peinfo.html')
+
+@app.route('/peview')
+def peview_e():
+    return render_template('peview.html')
+
+@app.route('/pack_protector', methods = ['GET','POST'])
 def render_file():
     return render_template('pack_protector.html')
-
 
 #파일 업로드 처리
 @app.route('/upload', methods = ['GET', 'POST'])
@@ -54,8 +63,9 @@ def upload_file():
 def pack_download_file():
    if request.method == 'POST':
     try:
-        path="/PEviewer-Packer-Protector-PEinfo-website/app/"+f.filename
+        path="/PEviewer-Packer-Protector-PEinfo-website/app/"+f.filename+".7z"
         os.system('upx '+f.filename)
+        os.system('7zr a '+f.filename+'.7z '+f.filename)
         return send_file(path,as_attachment=True)
     except Exception as e:
         return "Please upload the file first."
