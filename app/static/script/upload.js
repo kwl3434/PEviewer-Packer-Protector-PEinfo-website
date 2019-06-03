44c6db60951892e22d10937494d56a9e1d279ac0
root <root@hidehide.i0c5oqebmsxudcc5fjiavfmzth.syx.internal.cloudapp.net>
2019-06-03 13:13:26 +0000

    spinner

--- app/static/script/upload.js
+++ app/static/script/upload.js
@@ -1,43 +1,32 @@
 $(document).ready(function() {
+	$("#upload").click(function(){
+		$("#upload").submit( function(event) {
+			event.preventDefault();
+			var formData = new FormData($("#upload")[0]);
+			$.ajax({
+				xhr : function() {
+					var xhr = new window.XMLHttpRequest();
+					xhr.upload.addEventListener('progress', function(e) {
+						if (e.lengthComputable) {
+							console.log('Bytes Loaded: ' + e.loaded);
+							console.log('Total Size: ' + e.total);
+							console.log('Percentage Uploaded: ' + (e.loaded / e.total))
+							var percent = Math.round((e.loaded / e.total) * 100);
+							$('#progressBar').attr('aria-valuenow', percent).css('width', percent + '%').text(percent + '%');
+						}
 
-	$('form').on('submit', function(event) {
-
-		event.preventDefault();
-
-		var formData = new FormData($('form')[0]);
-
-		$.ajax({
-			xhr : function() {
-				var xhr = new window.XMLHttpRequest();
-
-				xhr.upload.addEventListener('progress', function(e) {
-
-					if (e.lengthComputable) {
-
-						console.log('Bytes Loaded: ' + e.loaded);
-						console.log('Total Size: ' + e.total);
-						console.log('Percentage Uploaded: ' + (e.loaded / e.total))
-
-						var percent = Math.round((e.loaded / e.total) * 100);
-
-						$('#progressBar').attr('aria-valuenow', percent).css('width', percent + '%').text(percent + '%');
-
-					}
-
-				});
-
-				return xhr;
-			},
-			type : 'POST',
-			url : '/upload',
-			data : formData,
-			processData : false,
-			contentType : false,
-			success : function() {
-				alert('File uploaded!');
-			}
+					});
+					return xhr;
+				},
+				type : 'POST',
+				url : '/upload',
+				data : formData,
+				processData : false,
+				contentType : false,
+				success : function() {
+					//alert('File uploaded!');
+				}
+			});
 		});
-
 	});
-
 });
--- app/upload.py
+++ app/upload.py
@@ -9,23 +9,31 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 
-from flask import Flask, render_template, request, make_response
-
+from flask import Flask, render_template, request, make_response, redirect, url_for, flash
+#from flaskext.uploads import UploadSet,configure_uploads,ALL
 from flask import send_file
 from werkzeug import secure_filename
 
 from functools import wraps, update_wrapper
 from datetime import datetime
 
+ALLOWED_EXTENSIONS = set(['exe'])
+
 app = Flask(__name__)
+
+#files = UploadSet('files',ALL)
+
+#app.config['UPLOADED_FILES_DEST'] = 'uploads'
+#configure_uploads(app, files)
+
+app.secret_key = 'dont tell anyone'
 main='main.html'
 pack='pack_protector'
 howto='howtouse.html'
 total='total.html'
-Data='peview1.html'
+#Data='peview1.html'
 peinfo='peinfo'
 peview='peview'
-context='context.html'
 dosheader='DOSHEADER.html'
 fileheader='FILEHEADER.html'
 sectionheadrsrc='SECTIONHEADRSRC.html'
@@ -35,13 +43,15 @@ sectionrsrc='SECTIONRSRC.html'
 sectionupx0='SECTIONUPX0.html'
 sectionupx1='SECTIONUPX1.html'
 
-
 jpg1='1.JPG'
 jpg2='2.JPG'
 jpg3='3.JPG'
 jpg4='4.JPG'
 jpg5='5.JPG'
-
+# exe파일만 허용하는 함수
+def allowed_file(filename): 
+        return '.' in filename and \
+                filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 #업로드 HTML 렌더링
 
 @app.route('/')
@@ -60,10 +70,6 @@ def home():
 def howtouse():
     return render_template('howtouse.html')
 
-@app.route('/context')
-def context():
-    return render_template('context.html')
-
 @app.route('/loading')
 def loading():
     return render_template('loading.html')
@@ -75,16 +81,19 @@ def peinfo_e():
 @app.route('/peview', methods = ['GET','POST'])
 def peview_e():
    return render_template('peview.html')
-	#return Response(f.read(), mimetype='text/plain')					 #data='peviewer'
+#return Response(f.read(), mimetype='text/plain')
+#data='peviewer'
 #def peview_ee(Data):
-   # return render_template('peview.html',data=Data)
+#return render_template('peview.html',data=Data)
 
-@app.route('/DOSHEADER', methods = ['GET', 'POST'])
+@app.route('/DOSHEADER')
 def dosheader():
 	return render_template('DOSHEADER.html')
-@app.route('/FILEHEADER', methods = ['GET', 'POST'])
+
+@app.route('/FILEHEADER')
 def fileheader():
 	return render_template('FILEHEADER.html')
+
 @app.route('/OPTIONALHEADER', methods = ['GET', 'POST'])
 def optionalheader():
 	return render_template('OPTIONALHEADER.html')
@@ -92,19 +101,24 @@ def optionalheader():
 @app.route('/SECTIONHEADRSRC', methods = ['GET', 'POST'])
 def sectionheadrsrc():
 	return render_template('SECTIONHEADRSRC.html')
+
 @app.route('/SECTIONHEADUPX0', methods = ['GET', 'POST'])
 def sectionheadupx0():
 	return render_template('SECTIONHEADUPX0.html')
+
 @app.route('/SECTIONHEADUPX1', methods = ['GET','POST'])
 def sectionheadupx1():
 	return render_template('SECTIONHEADUPX1.html')
 
+
 @app.route('/SECTIONUPX0', methods = ['GET','POST'])
 def sectionupx0():
 	return render_template('SECTIONUPX0.html')
+
 @app.route('/SECTIONUPX1')
 def sectionupx1():
 	return render_template('SECTIONUPX1.html')
+
 @app.route('/SECTIONRSRC', methods = ['GET','POST'])
 def sectionrsrc():
 	return render_template('SECTIONRSRC.html')
@@ -112,23 +126,26 @@ def sectionrsrc():
 
 @app.route('/pack_protector', methods = ['GET','POST'])
 def render_file():
+    message = "Warning : Please upload the exe file only. korean file name (X)".encode("utf-8", "ignore")
+    flash(message) 
+#flash 이용 업로드 경고문 처리
     return render_template('pack_protector.html',ff="Upload File..")
 
 #파일 업로드 처리
 @app.route('/upload', methods = ['GET', 'POST'])
 def upload_file():
+   #error = None
    if request.method == 'POST':
       f = request.files['file']
       #저장할 경로 + 파일명
-      f.save(secure_filename(f.filename))
+#try:
+   if f and allowed_file(f.filename):
+      #flash('Upload Successfully!') 
+      f.save(secure_filename(f.filename)) 
       os.system('./viruscheck '+f.filename)
-      #os.system('pehash -f html '+f.filename+'> ./templates/peview1.html ')
-      #os.system('readpe -f html '+f.filename+'>> ./templates/peview1.html ')
       os.system('pepack -f html '+f.filename+'> ./templates/peinfo1.html ')
       os.system('pesec -f html '+f.filename+'>> ./templates/peinfo1.html ')
-      #if f=
       os.system('readpe -f html -h dos '+f.filename+'> ./templates/DOSHEADER.html ')
-      #os.system(stubprogram)
       os.system('readpe -f html -h coff '+f.filename+'> ./templates/FILEHEADER.html ')
       os.system('readpe -f html -h optional '+f.filename+'>> ./templates/FILEHEADER.html ')
       os.system('readpe -f html -S UPX0 '+f.filename+'> ./templates/SECTIONHEADUPX0.html ')
@@ -136,22 +153,23 @@ def upload_file():
       os.system('readpe -f html -S .rsrc '+f.filename+'> ./templates/SECTIONHEADRSRC.html ')
       os.system('pehash -f html -s UPX0 '+f.filename+'> ./templates/SECTIONUPX0.html ')
       os.system('pehash -f html -s UPX1 '+f.filename+'> ./templates/SECTIONUPX1.html ')
-      os.system('pehash -f html -s .rsrc '+f.filename+'> ./templates/SECTIONRSRC.html ')
+      os.system('pehash -f html -s .rsrc '+f.filename+'> ./templates/SECTIONRSRC.html ')  
+      flash("Upload and Virus check complete!")
       TXT = open("/root/TorF.txt",'r');
       line = TXT.readline()
       TXT.close()
       if line=='None':
-	os.system('chown hide '+f.filename)
       	return render_template('pack_protector.html',ff=f.filename)
       else:
+        error = 'Please exe file upload!'
 	os.system('rm '+f.filename);
-	return "Virus detected"
-
+	return "Virus detected" 
+    
 @app.route('/pack_download', methods = ['GET', 'POST'])
 def pack_download_file():
    if request.method == 'POST':
 	f=request.form['fname']
-	if f=="Upload":
+	if f=="Upload File..":
 		return "Please Upload File first"
 	elif f[0]=='/' or f[0]=='.' or f[0]==' ' or f[0]==';':
 		return "Warning 112"
@@ -159,6 +177,7 @@ def pack_download_file():
   		path="/PEviewer-Packer-Protector-PEinfo-website/app/"+f+".7z"
         	os.system('upx '+f)
 	        os.system('7z a '+f+'.7z '+f)
+                flash("Pack and download complete!")
         	return send_file(path,as_attachment=True)
 
 @app.route('/unpack_download', methods = ['GET', 'POST'])
@@ -169,7 +188,7 @@ def unpack_download_file():
 		return "Please Upload File first"
 	elif f[0]=='/' or f[0]=='.' or f[0]==' ' or f[0]==';':
 		return "Warning 112"
-	else:
+        else:
         	path="/PEviewer-Packer-Protector-PEinfo-website/app/"+f+".7z"
         	os.system('upx -d '+f)
         	os.system('7z a '+f+'.7z '+f)
@@ -179,7 +198,7 @@ def unpack_download_file():
 def protect_download_file():
    if request.method == 'POST':
 	f=request.form['fname']
-	if f=="Upload":
+	if f=="Upload File..":
 		return "Please Upload File first"
 	elif f[0]=='/' or f[0]=='.' or f[0]==' ' or f[0]==';':
 		return "Warning 112"
